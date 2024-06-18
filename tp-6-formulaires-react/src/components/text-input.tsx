3698a5b1b2735a4c3d79c5294e25968828188b08
Dacta <92741898+D4cta@users.noreply.github.com>
2024-06-18 11:50:20 +0200

    TP

---
+++ tp-6-formulaires-react/src/components/text-input.tsx
@@ -0,0 +1,13 @@
+import { FC } from "react"
+
+
+function Label() {
+	return <>Mon composant !</>
+}
+
+function TextInput : FC<{initialValue : string, placeholder : string, onChange : string, label : string, required : boolean}>() {
+	return <>Mon composant !</>
+}
+
+
+export default TextInput; Label
---
+++ tp-6-formulaires-react/src/exercices/exercice-1.tsx
@@ -0,0 +1,8 @@
+
+
+function Composant_exo1() {
+	return <>Mon composant !</>
+}
+
+
+export default Composant_exo1
